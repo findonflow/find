71584f27c00550f217fe2f9f89867ef0bcb90229
Bjarte Karlsen <bjarte@bjartek.org>
2021-09-14 23:00:42 +0200

    delete cadut and add cadence-to-json

---
+++ build.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const transactionsPath = path.join(__dirname, 'transactions', '/')
+const scriptsPath = path.join(__dirname,  'scripts', '/')
+
+const convertCadenceToJs = async () => {
+    const resultingJs = await require('cadence-to-json')({
+        transactions: [ transactionsPath ],
+        scripts: [ scriptsPath ],
+        config: require('./flow.json')
+    })
+    fs.writeFile('lib/find.json', JSON.stringify(resultingJs), (err) => {
+        if (err) {
+            console.error("Failed to read CadenceToJs JSON");
+            process.exit(1)
+        }
+    })
+}
+
+convertCadenceToJs()
