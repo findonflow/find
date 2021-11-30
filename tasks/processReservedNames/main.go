45becc8a4e9cf3a2046e29f120ba86add5f89dca
Bjarte Stien Karlsen <bjarte@bjartek.org>
2021-11-30 13:56:34 +0100

    added proceddReservedNames job

--- tasks/processReservedNames/main.go
+++ tasks/processReservedNames/main.go
@@ -1,17 +1,81 @@
 package main
 
 import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/bjartek/go-with-the-flow/v2/gwtf"
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 
-	adminName := "find"
+	file, ok := os.LookupEnv("file")
+	if !ok {
+		fmt.Println("file is not present")
+		os.Exit(1)
+	}
+
 	g := gwtf.NewGoWithTheFlowEmulator()
 
-	result := g.ScriptFromFile("reserveStatus").AccountArgument(adminName).RunReturnsInterface()
-	spew.Dump(result)
-	// run script to fetch all names with direct offers
+	reservedNames := readNameAddresses(file)
+	result := g.ScriptFromFile("reserveStatus").AccountArgument("find").RunReturnsJsonString()
+	var bids []LeaseBids
+	err := json.Unmarshal([]byte(result), &bids)
+	if err != nil {
+		panic(err)
+	}
 
+	for _, bid := range bids {
+		reservedAddress, exist := reservedNames[bid.Name]
+		if !exist {
+			fmt.Sprintf("Name is not reserved %s\n", bid.Name)
+			continue
+		}
+
+		if bid.LatestBidBy == reservedAddress {
+			g.TransactionFromFile("fulfill").SignProposeAndPayAs("find").StringArgument(bid.Name).RunPrintEventsFull()
+		} else {
+			g.TransactionFromFile("rejectDirectOffer").SignProposeAndPayAs("find").StringArgument(bid.Name).RunPrintEventsFull()
+		}
+	}
+
+	g.ScriptFromFile("name_status").StringArgument("test").Run()
+
+}
+
+type LeaseBids struct {
+	Address     string `json:"address"`
+	Cost        string `json:"cost"`
+	LatestBid   string `json:"latestBid"`
+	LatestBidBy string `json:"latestBidBy"`
+	Name        string `json:"name"`
+	Status      string `json:"status"`
+}
+
+func readNameAddresses(filePath string) map[string]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	results := map[string]string{}
+	for i, row := range records {
+		if i == 0 {
+			continue
+		}
+		results[row[3]] = row[2]
+	}
+
+	return results
 }
