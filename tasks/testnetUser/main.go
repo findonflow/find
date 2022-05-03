f256b72a6bc4fddded42426b2ab59f2161440fe9
Bjarte Karlsen <bjarte@bjartek.org>
2022-05-03 18:17:39 +0200

    fixed new testnet address because of braking data change

--- tasks/testnetUser/main.go
+++ tasks/testnetUser/main.go
@@ -8,32 +8,22 @@ func main() {
 
 	o := overflow.NewOverflowTestnet().Start()
 
-	/*
-			o.TransactionFromFile("createProfile").
-				SignProposeAndPayAsService().
-				Args(o.Arguments().String("find")).
-				RunPrintEventsFull()
-
-		o.TransactionFromFile("register").
-			SignProposeAndPayAs("user1").
-			Args(o.Arguments().String("user1").UFix64(5.0)).
-			RunPrintEventsFull()
-
-
-	*/
+	o.SimpleTxArgs("adminSendFUSD", "account", o.Arguments().Account("user1").UFix64(100.0))
 
 	o.TransactionFromFile("buyAddon").SignProposeAndPayAs("user1").
 		Args(o.Arguments().String("user1").String("forge").UFix64(50.0)).
 		RunPrintEventsFull()
 
-	o.TransactionFromFile("mintDandy").
+	user := "0x3caf1997d883ca97"
+	o.TransactionFromFile("mintDandyTO").
 		SignProposeAndPayAs("user1").
 		Args(o.Arguments().
 			String("user1").
-			UInt64(3).
+			UInt64(5).
 			String("Neo").
 			String("Neo Motorcycle").
 			String(`Bringing the motorcycle world into the 21st century with cutting edge EV technology and advanced performance in a great classic British style, all here in the UK`).
-			String("https://neomotorcycles.co.uk/assets/img/neo_motorcycle_side.webp")).
+			String("https://neomotorcycles.co.uk/assets/img/neo_motorcycle_side.webp").
+			RawAddress(user)).
 		RunPrintEventsFull()
 }
