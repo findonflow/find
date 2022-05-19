4489fea1d35246c0424039c54581f0856b5b40ce
Bjarte Stien Karlsen <bjarte@bjartek.org>
2022-05-19 14:47:32 +0200

    clean up client

---
+++ tasks/client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/bjartek/overflow/overflow"
+)
+
+func main() {
+	g := overflow.NewOverflow().ExistingEmulator().Start()
+
+	res, err := g.ParseAllWithConfig(true, []string{
+		"^setup_*",
+		"^mint*",
+		"clock",
+		"registerAdmin",
+		"adminSendFlow",
+		"transferAllFusd",
+		"fillUpTheChest",
+		"setSellDandyForFlow.cdc",
+	}, []string{})
+	if err != nil {
+		panic(err)
+	}
+
+	merged := res.MergeSpecAndCode()
+
+	file, _ := json.MarshalIndent(merged, "", "   ")
+	fmt.Println(string(file))
+
+}
