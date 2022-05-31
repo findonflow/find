b372b337e1168129018d4e7debe426b50a6c5bdd
Bjarte Stien Karlsen <bjarte@bjartek.org>
2022-05-31 10:39:54 +0200

    use our own MetadataViews for now since testnet is not updated

--- tasks/testnet/main.go
+++ tasks/testnet/main.go
@@ -9,14 +9,16 @@ func main() {
 	//g := gwtf.NewGoWithTheFlowEmulator().InitializeContracts().CreateAccounts("emulator-account")
 	o := overflow.NewOverflowTestnet().Start()
 
-	o.SimpleTxArgs("testSendFlow", "account", o.Arguments().Account("find").UFix64(1.0))
+	/*
+		o.SimpleTxArgs("adminSendFlow", "account", o.Arguments().Account("find").UFix64(1.0))
 
-	//Deploy contracts to testnet
-	o.InitializeContracts()
+		//Deploy contracts to testnet
+		o.InitializeContracts()
+	*/
 
 	//send flow to admin and user account
-	o.SimpleTxArgs("testSendFlow", "account", o.Arguments().Account("find-admin").UFix64(1000.0))
-	o.SimpleTxArgs("testSendFlow", "account", o.Arguments().Account("user1").UFix64(1000.0))
+	o.SimpleTxArgs("adminSendFlow", "account", o.Arguments().Account("find-admin").UFix64(1000.0))
+	o.SimpleTxArgs("adminSendFlow", "account", o.Arguments().Account("user1").UFix64(1000.0))
 
 	//first step create the adminClient as the fin user
 	o.TransactionFromFile("setup_fin_1_create_client").
@@ -43,10 +45,7 @@ func main() {
 		SignProposeAndPayAs("find-admin").
 		Args(o.Arguments().Account("find")).
 		RunPrintEventsFull()
-		//n
-		//		./transactions/setNFTInfo_Dandy.cdc
 
-	o.SimpleTxArgs("adminSetupMarketOptionsTypes", "find-admin", o.Arguments())
 	o.SimpleTxArgs("adminSetFTInfo_flow", "find-admin", o.Arguments())
 	o.SimpleTxArgs("adminSetFTInfo_usdc", "find-admin", o.Arguments())
 	o.SimpleTxArgs("adminSetFTInfo_fusd", "find-admin", o.Arguments())
@@ -64,7 +63,7 @@ func main() {
 	createProfileAndGiftName(o, "find-admin")
 	createProfileAndGiftName(o, "user1")
 
-	o.SimpleTxArgs("testSendFUSD", "account", o.Arguments().Account("user1").UFix64(100.0))
+	o.SimpleTxArgs("adminSendFUSD", "account", o.Arguments().Account("user1").UFix64(100.0))
 
 	o.TransactionFromFile("buyAddon").SignProposeAndPayAs("user1").
 		Args(o.Arguments().String("user1").String("forge").UFix64(50.0)).
