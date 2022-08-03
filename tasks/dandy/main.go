00536c610f5ec08448a09cb0d1574d44e147cf3c
Bjarte S. Karlsen <bjarte@bjartek.org>
2022-08-03 13:39:01 +0200

    Bjartek/find 352 add forge to find name and mint a dandy (#179)
    * started on seting up a dandy forge and minting to it on mainnet for find.find
    
    * fixed on mint task
    
    * Add Dandy to Find NFT Catalog (#177)
    
    * Add Dandy to Find NFT Catalog
    
    * Change identifier
    
    Co-authored-by: Hui Ben <ben@find.xyz>
    
    * Ben/deploy findnft catalog (#180)
    
    * Add Dandy to Find NFT Catalog
    
    * Change identifier
    
    * Deploy FindNFTCatalog
    
    Co-authored-by: Hui Ben <ben@find.xyz>
    
    Co-authored-by: Bamthelearner <95492891+Bamthelearner@users.noreply.github.com>
    Co-authored-by: Hui Ben <ben@find.xyz>

---
+++ tasks/dandy/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	. "github.com/bjartek/overflow"
+)
+
+func main() {
+
+	o := Overflow(WithNetwork("mainnet"), WithGlobalPrintOptions())
+
+	findSigner := WithSigner("find")
+	nameArg := WithArg("name", "find")
+
+	//should these two join together?
+	o.Tx("buyAddon", findSigner, nameArg, WithArg("addon", "forge"), WithArg("amount", 50.0))
+	o.Tx("setupDandy", findSigner, nameArg,
+		WithArg("minterCut", 0.025),
+		WithArg("description", ".find and dandy nfts"),
+		WithArg("externalURL", "http://find.xyz/find"),
+		WithArg("squareImage", "https://pbs.twimg.com/profile_images/1467546091780550658/R1uc6dcq_400x400.jpg"),
+		WithArg("bannerImage", "https://pbs.twimg.com/profile_banners/1448245049666510848/1652452073/1500x500"),
+		WithArg("socials", `{ "Twitter" : "https://twitter.com/findonflow" , "Discord" : "https://discord.gg/95P274mayM" }`),
+	)
+
+	id, err := o.Tx("adminMintFindDandy",
+		findSigner,
+		nameArg,
+		WithArg("maxEdition", 100),
+		WithArg("nftName", "Find OG Mint"),
+		WithArg("nftDescription", "A collection of 100 find mints to commemorate the first ever minted NFT on the .find platform"),
+		WithArg("nftImage", "https://cdn.discordapp.com/attachments/870316419521335356/1004130351020523560/find1500.jpg"),
+	).GetIdFromEvent(
+		"Minted", "id",
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Add Dandy to FIND NFT Catalog
+	o.Tx("adminAddNFTCatalog",
+		findSigner,
+		WithArg("collectionIdentifier", "A.097bafa4e0b48eef.Dandy.NFT"),
+		WithArg("contractName", "Dandy"),
+		WithArg("contractAddress", "find"),
+		WithArg("addressWithNFT", "find"),
+		WithArg("nftID", id),
+		WithArg("publicPathIdentifier", "findDandy"),
+	)
+
+}
+
+/*
+				//first step create the adminClient as the fin user
+				g.TransactionFromFile("setup_fin_1_create_client").
+					SignProposeAndPayAs("find-admin").
+					RunPrintEventsFull()
+
+			//link in the server in the versus client
+			o.TransactionFromFile("setup_fin_2_register_client").
+				SignProposeAndPayAs("find").
+				Args(o.Arguments().Account("find-admin")).
+				RunPrintEventsFull()
+
+					//set up fin network as the fin user
+					g.TransactionFromFile("setup_fin_3_create_network").
+						SignProposeAndPayAs("find-admin").
+						RunPrintEventsFull()
+
+			o.TransactionFromFile("createProfile").
+				SignProposeAndPayAs("find").
+				Args(o.Arguments().String("find")).
+				RunPrintEventsFull()
+
+			findLinks := cadence.NewArray([]cadence.Value{
+				cadence.NewDictionary([]cadence.KeyValuePair{
+					{Key: NewCadenceSting("title"), Value: NewCadenceSting("twitter")},
+					{Key: NewCadenceSting("type"), Value: NewCadenceSting("twitter")},
+					{Key: NewCadenceSting("url"), Value: NewCadenceSting("https://twitter.com/findonflow")},
+				})})
+
+			o.TransactionFromFile("createProfile").
+				SignProposeAndPayAs("find-admin").
+				Args(o.Arguments().String("ReservedNames")).
+				RunPrintEventsFull()
+
+			o.TransactionFromFile("editProfile").
+				SignProposeAndPayAs("find-admin").
+				Args(o.Arguments().
+					String("ReservedFindNames").
+					String(`The names owned by this profile are reservd by .find. In order to aquire a name here you have to:
+
+		Prices:
+		 - 3 letter name  500 FUSD
+		 - 4 letter name  100 FUSD
+		 - 5+ letter name   5 FUSD
+
+		1. make an offer for that name with the correct price (see above)
+		2. go into the find discord and let the mods know you have made the bid
+
+		`).
+					String("https://find.xyz/find.png").
+					StringArray("find").
+					Boolean(false).
+					Argument(findLinks)).
+				RunPrintEventsFull()
+
+			o.TransactionFromFile("editProfile").
+				SignProposeAndPayAs("find").
+				Args(o.Arguments().
+					String("find").
+					String(`.find will allow you to find people and NFTS on flow!`).
+					String("https://find.xyz/find.png").
+					StringArray("find").
+					Boolean(true).
+					Argument(findLinks)).
+				RunPrintEventsFull()
+
+			o.TransactionFromFile("registerAdmin").
+				SignProposeAndPayAs("find-admin").
+				Args(o.Arguments().StringArray("find").Account("find")).
+				RunPrintEventsFull()
+
+			o.TransactionFromFile("registerAdmin").
+				SignProposeAndPayAs("find-admin").
+				Args(o.Arguments().StringArray("reserved-names").Account("find-admin")).
+				RunPrintEventsFull()
+
+}
+*/
---
+++ transactions/adminMintFindDandy.cdc
@@ -0,0 +1,50 @@
+
+import FIND from "../contracts/FIND.cdc"
+import NonFungibleToken from "../contracts/standard/NonFungibleToken.cdc"
+import FungibleToken from "../contracts/standard/FungibleToken.cdc"
+import Dandy from "../contracts/Dandy.cdc"
+import Profile from "../contracts/Profile.cdc"
+import MetadataViews from "../contracts/standard/MetadataViews.cdc"
+import FindViews from "../contracts/FindViews.cdc"
+import FindForge from "../contracts/FindForge.cdc"
+
+transaction(name: String, maxEdition:UInt64, nftName:String, nftDescription:String, nftImage:String) {
+	prepare(account: AuthAccount) {
+
+		let finLeases= account.borrow<&FIND.LeaseCollection>(from:FIND.LeaseStoragePath)!
+		let lease=finLeases.borrow(name)
+		let forgeType = Dandy.getForgeType()
+
+		let dandyCap= account.getCapability<&{NonFungibleToken.CollectionPublic}>(Dandy.CollectionPublicPath)
+		let httpFile=MetadataViews.HTTPFile(url:nftImage)
+		let media=MetadataViews.Media(file: httpFile, mediaType: "image/png")
+
+		let receiver=account.getCapability<&{FungibleToken.Receiver}>(Profile.publicReceiverPath)
+		let nftReceiver=account.getCapability<&{NonFungibleToken.Receiver, MetadataViews.ResolverCollection}>(Dandy.CollectionPublicPath).borrow() ?? panic("Cannot borrow reference to Dandy collection.")
+
+		let traits = MetadataViews.Traits([])
+		traits.addTrait(MetadataViews.Trait(name: "forger", value: "Find", displayType:"String", rarity:nil))
+
+		let collection=dandyCap.borrow()!
+		var i:UInt64=1
+
+		while i <= maxEdition {
+
+			let editioned= MetadataViews.Edition(name: nil, number:i, max:maxEdition)
+			let set= MetadataViews.Edition(name: "set", number:i, max:maxEdition)
+			let editions = MetadataViews.Editions([editioned, set])
+			let schemas: [AnyStruct] = [ editions, MetadataViews.Medias([media]), traits ]
+			
+			let mintData = Dandy.DandyInfo(name: nftName, 
+												description: nftDescription, 
+												thumbnail: media, 
+												schemas: schemas, 
+												externalUrlPrefix:"https://find.xyz/".concat(name).concat("/Catalog/Dandy"))
+			
+			FindForge.mint(lease: lease, forgeType: forgeType, data: mintData, receiver: nftReceiver)
+		
+			i=i+1
+		}
+
+	}
+}
