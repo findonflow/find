18aec9c7469c06a8861628903deb9751a4433790
Bjarte Stien Karlsen <bjarte@bjartek.org>
2022-08-05 11:05:53 +0200

    added sale items

--- tasks/mainnetMarket/main.go
+++ tasks/mainnetMarket/main.go
@@ -7,99 +7,50 @@ import (
 func main() {
 
 	adminSigner := WithSigner("find-admin")
-	tenant := WithArg("tenant", "find")
-	ftName := WithArg("ftName", "flow")
-	ft := WithArg("ftTypes", `["A.1654653399040a61.FlowToken.Vault"]`)
-	listingName := WithArg("listingName", "escrow")
-	listing := WithArg("listingTypes", `["A.097bafa4e0b48eef.FindMarketSale.SaleItem" , "A.097bafa4e0b48eef.FindMarketAuctionEscrow.SaleItem" , "A.097bafa4e0b48eef.FindMarketDirectOfferEscrow.SaleItem"]`)
 
 	o := Overflow(
 		WithNetwork("mainnet"),
 		WithGlobalPrintOptions(),
 	)
 
-	o.Tx("adminSetFTInfo_flow",
+	/*
+		o.Tx("adminSetFTInfo_flow",
+			adminSigner,
+		)
+
+		o.Tx("adminSetFTInfo_usdc",
+			adminSigner,
+		)
+
+		o.Tx("adminSetFTInfo_fusd",
+			adminSigner,
+		)
+	*/
+
+	upsertItem := o.TxFileNameFN("adminMainnetAddItem",
 		adminSigner,
+		WithArg("tenant", "find"),
+		WithArg("ftName", "flow"),
+		WithArg("ftTypes", `["A.1654653399040a61.FlowToken.Vault"]`),
+		WithArg("listingName", "escrow"),
+		WithArg("listingTypes", `["A.097bafa4e0b48eef.FindMarketSale.SaleItem" , "A.097bafa4e0b48eef.FindMarketAuctionEscrow.SaleItem" , "A.097bafa4e0b48eef.FindMarketDirectOfferEscrow.SaleItem"]`),
 	)
 
-	o.Tx("adminSetFTInfo_usdc",
-		adminSigner,
-	)
+	flowNfts := map[string]string{
+		"bl0x":       `["A.7620acf6d7f2468a.Bl0x.NFT"]`,
+		"pharaohs":   `["A.9d21537544d9123d.Momentables.NFT"]`,
+		"versus":     `["A.d796ff17107bbff6.Art.NFT"]`,
+		"flovatar":   `["A.921ea449dffec68a.Flovatar.NFT" , "A.921ea449dffec68a.FlovatarComponent.NFT"]`,
+		"neoCharity": `["A.097bafa4e0b48eef.CharityNFT.NFT"]`,
+		"starly":     `["A.5b82f21c0edf76e3.StarlyCard.NFT"]`,
+		"float":      `["A.2d4c3caffbeab845.FLOAT.NFT"]`,
+		"dayNFT":     `["A.1600b04bf033fb99.DayNFT.NFT"]`,
+	}
 
-	o.Tx("adminSetFTInfo_fusd",
-		adminSigner,
-	)
-
-	// Bl0x
-	o.Tx("adminMainnetAddItem",
-		adminSigner,
-		tenant,
-		ftName,
-		ft,
-		WithArg("nftName", "bl0x"),
-		WithArg("nftTypes", `["A.7620acf6d7f2468a.Bl0x.NFT"]`),
-		listingName,
-		listing,
-	)
-
-	// Pharaohs
-	o.Tx("adminMainnetAddItem",
-		adminSigner,
-		tenant,
-		ftName,
-		ft,
-		WithArg("nftName", "pharaohs"),
-		WithArg("nftTypes", `["A.9d21537544d9123d.Momentables.NFT"]`),
-		listingName,
-		listing,
-	)
-
-	// Versus
-	o.Tx("adminMainnetAddItem",
-		adminSigner,
-		tenant,
-		ftName,
-		ft,
-		WithArg("nftName", "versus"),
-		WithArg("nftTypes", `["A.d796ff17107bbff6.Art.NFT"]`),
-		listingName,
-		listing,
-	)
-
-	// Flovatar
-	o.Tx("adminMainnetAddItem",
-		adminSigner,
-		tenant,
-		ftName,
-		ft,
-		WithArg("nftName", "flovatar"),
-		WithArg("nftTypes", `["A.921ea449dffec68a.Flovatar.NFT" , "A.921ea449dffec68a.FlovatarComponent.NFT"]`),
-		listingName,
-		listing,
-	)
-
-	// Neo Charity
-	o.Tx("adminMainnetAddItem",
-		adminSigner,
-		tenant,
-		ftName,
-		ft,
-		WithArg("nftName", "neoCharity"),
-		WithArg("nftTypes", `["A.097bafa4e0b48eef.CharityNFT.NFT"]`),
-		listingName,
-		listing,
-	)
-
-	// Starly
-	o.Tx("adminMainnetAddItem",
-		adminSigner,
-		tenant,
-		ftName,
-		ft,
-		WithArg("nftName", "starly"),
-		WithArg("nftTypes", `["A.5b82f21c0edf76e3.StarlyCard.NFT"]`),
-		listingName,
-		listing,
-	)
-
+	for name, contracts := range flowNfts {
+		upsertItem(
+			WithArg("nftName", name), //primary key
+			WithArg("nftTypes", contracts),
+		)
+	}
 }
