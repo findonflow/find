441699f64f7015147743559d19c2fb60fe4d591a
Hui Ben <ben@find.xyz>
2022-08-11 17:11:19 +0800

    Uncomment

--- tasks/collectionDataTest/main.go
+++ tasks/collectionDataTest/main.go
@@ -108,63 +108,63 @@ func main() {
 		WithArg("collectionIDs", `{"schmoes_prelaunch_token" : [9]}`),
 	)
 
-	// // get NFTDetail script
-	// script = "getNFTDetails"
+	// get NFTDetail script
+	script = "getNFTDetails"
 
-	// suffix = "RaribleNFT"
-	// o.Script(prefix+script+suffix,
-	// 	WithArg("user", "bjartek"),
-	// 	WithArg("project", "Flowverse Socks"),
-	// 	WithArg("id", 14939),
-	// 	WithArg("views", "[]"),
-	// )
+	suffix = "RaribleNFT"
+	o.Script(prefix+script+suffix,
+		WithArg("user", "bjartek"),
+		WithArg("project", "Flowverse Socks"),
+		WithArg("id", 14939),
+		WithArg("views", "[]"),
+	)
 
-	// suffix = "Shard1"
-	// o.Script(prefix+script+suffix,
-	// 	WithArg("user", "bjartek"),
-	// 	WithArg("project", "TuneGO"),
-	// 	WithArg("id", 382),
-	// 	WithArg("views", "[]"),
-	// )
+	suffix = "Shard1"
+	o.Script(prefix+script+suffix,
+		WithArg("user", "bjartek"),
+		WithArg("project", "TuneGO"),
+		WithArg("id", 382),
+		WithArg("views", "[]"),
+	)
 
-	// suffix = "Shard2"
-	// o.Script(prefix+script+suffix,
-	// 	WithArg("user", "bjartek"),
-	// 	WithArg("project", "GeniaceNFT"),
-	// 	WithArg("id", 2083),
-	// 	WithArg("views", "[]"),
-	// )
+	suffix = "Shard2"
+	o.Script(prefix+script+suffix,
+		WithArg("user", "bjartek"),
+		WithArg("project", "GeniaceNFT"),
+		WithArg("id", 2083),
+		WithArg("views", "[]"),
+	)
 
-	// suffix = "Shard3"
-	// o.Script(prefix+script+suffix,
-	// 	WithArg("user", "bjartek"),
-	// 	WithArg("project", "BlindBoxRedeemVoucher"),
-	// 	WithArg("id", 38477),
-	// 	WithArg("views", "[]"),
-	// )
+	suffix = "Shard3"
+	o.Script(prefix+script+suffix,
+		WithArg("user", "bjartek"),
+		WithArg("project", "BlindBoxRedeemVoucher"),
+		WithArg("id", 38477),
+		WithArg("views", "[]"),
+	)
 
-	// suffix = "Shard4"
-	// o.Script(prefix+script+suffix,
-	// 	WithArg("user", "bjartek"),
-	// 	WithArg("project", "PartyMansionDrinksContract"),
-	// 	WithArg("id", 4034),
-	// 	WithArg("views", "[]"),
-	// )
+	suffix = "Shard4"
+	o.Script(prefix+script+suffix,
+		WithArg("user", "bjartek"),
+		WithArg("project", "PartyMansionDrinksContract"),
+		WithArg("id", 4034),
+		WithArg("views", "[]"),
+	)
 
-	// suffix = "NFTCatalog"
-	// o.Script(script+suffix,
-	// 	WithArg("user", "bjartek"),
-	// 	WithArg("project", "A.921ea449dffec68a.Flovatar.NFT"),
-	// 	WithArg("id", 2271),
-	// 	WithArg("views", "[]"),
-	// )
+	suffix = "NFTCatalog"
+	o.Script(script+suffix,
+		WithArg("user", "bjartek"),
+		WithArg("project", "A.921ea449dffec68a.Flovatar.NFT"),
+		WithArg("id", 2271),
+		WithArg("views", "[]"),
+	)
 
-	// // if that item is soul bounded , it will not show on allow listing
-	// o.Script(script+suffix,
-	// 	WithArg("user", "bjartek"),
-	// 	WithArg("project", "FLOAT"),
-	// 	WithArg("id", 277927096),
-	// 	WithArg("views", "[]"),
-	// )
+	// if that item is soul bounded , it will not show on allow listing
+	o.Script(script+suffix,
+		WithArg("user", "bjartek"),
+		WithArg("project", "FLOAT"),
+		WithArg("id", 277927096),
+		WithArg("views", "[]"),
+	)
 
 }
