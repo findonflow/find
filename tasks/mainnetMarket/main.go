b8e89a82df800644f0868c6d1799914375ffc70c
Bamthelearner <95492891+Bamthelearner@users.noreply.github.com>
2022-09-16 20:57:12 +0800

    Ben/find 350 reach out to projects and help them with (#222)
    * Add New Catalog Items
    
    * Add New Market Items
    
    Co-authored-by: Hui Ben <ben@find.xyz>

--- tasks/mainnetMarket/main.go
+++ tasks/mainnetMarket/main.go
@@ -67,17 +67,17 @@ func main() {
 				WithArg("nftID", 164769803),
 				WithArg("publicPathIdentifier", "somePlaceCollectibleCollection"),
 			)
-	*/
 
-	o.Tx("adminAddNFTCatalog",
-		WithSigner("find"),
-		WithArg("collectionIdentifier", "bl0xPack"),
-		WithArg("contractName", "Bl0xPack"),
-		WithArg("contractAddress", "0x7620acf6d7f2468a"),
-		WithArg("addressWithNFT", "0x0893d4423f25c7d6"),
-		WithArg("nftID", 208638414),
-		WithArg("publicPathIdentifier", "Bl0xPackCollection"),
-	)
+			o.Tx("adminAddNFTCatalog",
+				WithSigner("find"),
+				WithArg("collectionIdentifier", "bl0xPack"),
+				WithArg("contractName", "Bl0xPack"),
+				WithArg("contractAddress", "0x7620acf6d7f2468a"),
+				WithArg("addressWithNFT", "0x0893d4423f25c7d6"),
+				WithArg("nftID", 208638414),
+				WithArg("publicPathIdentifier", "Bl0xPackCollection"),
+			)
+	*/
 
 	upsertItem := o.TxFileNameFN("adminMainnetAddItem",
 		adminSigner,
@@ -93,7 +93,20 @@ func main() {
 		//	"Bitku":    `["A.f61e40c19db2a9e2.HaikuNFT.NFT"]`,
 		//		"Dandy": `["A.097bafa4e0b48eef.Dandy.NFT"]`,
 		// "some.place": `["A.667a16294a089ef8.SomePlaceCollectible.NFT"]`,
-		"bl0xPack": `["A.7620acf6d7f2468a.Bl0xPack.NFT"]`,
+		// "bl0xPack": `["A.7620acf6d7f2468a.Bl0xPack.NFT"]`,
+		"TheKrikeySolarpupsCollection": `["A.a8d493db1bb4df56.SolarpupsNFT.NFT"]`,
+		"DisruptArt":                   `["A.cd946ef9b13804c6.DisruptArt.NFT"]`,
+		"SequelDigitalArt":             `["A.3cb7ceeb625a600a.DigitalArt.NFT"]`,
+		"Yahoo":                        `["A.758252ab932a3416.YahooPartnersCollectible.NFT", "A.758252ab932a3416.YahooCollectible.NFT"]`,
+		"MonoCats":                     `["A.8529aaf64c168952.MonoCat.NFT", "A.8529aaf64c168952.MonoCatMysteryBox.NFT"]`,
+		"YBees":                        `["A.20187093790b9aef.YBees.NFT"]`,
+		"RCRDSHP":                      `["A.6c3ff40b90b928ab.RCRDSHPNFT.NFT"]`,
+		"CryptoPiggoNFTCollection":     `["A.d3df824bf81910a4.CryptoPiggo.NFT"]`,
+		"FlowverseSocks":               `["A.ce4c02539d1fabe8.FlowverseSocks.NFT"]`,
+		"Zeedz":                        `["A.62b3063fbe672fc8.ZeedzINO.NFT"]`,
+
+		// This will overwrite the old SoulMade
+		"SoulMade": `["A.9a57dfe5c8ce609c.SoulMadeComponent.NFT", "A.9a57dfe5c8ce609c.SoulMadeMain.NFT","A.9a57dfe5c8ce609c.SoulMadePack.NFT"]`,
 	}
 
 	for name, contracts := range flowNfts {
