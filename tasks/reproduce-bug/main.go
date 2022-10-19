7e71b2fbd2ac976b31a49a714d8d860dab8339a6
Hui Ben <ben@find.xyz>
2022-10-19 16:19:36 +0800

    edit text

--- tasks/reproduce-bug/main.go
+++ tasks/reproduce-bug/main.go
@@ -18,6 +18,6 @@ func main() {
 
 	fmt.Println("The keys returned as shown above.")
 	fmt.Println("Keys returned are different between 2 versions of overflow (i.e. 2 versions of onflow/cadence)")
-	fmt.Println("If you are on overflow@v1.0.0 , run make update-overflow and run this task again.")
+	fmt.Println("If you are on overflow@v1.0.0 , run `make update-overflow` and run this task again.")
 
 }
