8896e953b669f8c057a6e4dddf155be8a59287fc
Hui Ben <ben@find.xyz>
2022-10-28 14:25:33 +0800

    add soul bounded to getItem script

--- collection_test.go
+++ collection_test.go
@@ -1,52 +1,89 @@
 package test_main
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/bjartek/overflow"
 	"github.com/hexops/autogold"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCollectionScripts(t *testing.T) {
 
-	t.Run("Should be able to mint Dandy and then get it by script", func(t *testing.T) {
-		otu := NewOverflowTest(t)
+	otu := NewOverflowTest(t)
 
-		otu.setupFIND().
-			createUser(10000.0, "user1").
-			registerUser("user1").
-			buyForge("user1").
-			registerUserWithNameAndForge("user1", "neomotorcycle").
-			registerUserWithNameAndForge("user1", "xtingles").
-			registerUserWithNameAndForge("user1", "flovatar").
-			registerUserWithNameAndForge("user1", "ufcstrike").
-			registerUserWithNameAndForge("user1", "jambb").
-			registerUserWithNameAndForge("user1", "bitku").
-			registerUserWithNameAndForge("user1", "goatedgoats").
-			registerUserWithNameAndForge("user1", "klktn")
+	otu.setupFIND().
+		createUser(10000.0, "user1").
+		registerUser("user1").
+		buyForge("user1").
+		registerUserWithNameAndForge("user1", "neomotorcycle").
+		registerUserWithNameAndForge("user1", "xtingles").
+		registerUserWithNameAndForge("user1", "flovatar").
+		registerUserWithNameAndForge("user1", "ufcstrike").
+		registerUserWithNameAndForge("user1", "jambb").
+		registerUserWithNameAndForge("user1", "bitku").
+		registerUserWithNameAndForge("user1", "goatedgoats").
+		registerUserWithNameAndForge("user1", "klktn")
 
-		otu.setUUID(500)
+	otu.setUUID(500)
 
-		otu.O.Tx("testMintDandyTO",
-			overflow.WithSigner("user1"),
-			overflow.WithArg("name", "user1"),
-			overflow.WithArg("maxEdition", 1),
-			overflow.WithArg("artist", "Neo"),
-			overflow.WithArg("nftName", "Motorcycle"),
-			overflow.WithArg("nftDescription", `Bringing the motorcycle world into the 21st century with cutting edge EV technology and advanced performance in a great classic British style, all here in the UK`),
-			overflow.WithArg("nftUrl", "https://neomotorcycles.co.uk/assets/img/neo_motorcycle_side.webp"),
-			overflow.WithArg("rarity", "rare"),
-			overflow.WithArg("rarityNum", 50.0),
-			overflow.WithArg("to", "user1"),
-		).
-			AssertSuccess(t)
+	otu.O.Tx("testMintDandyTO",
+		overflow.WithSigner("user1"),
+		overflow.WithArg("name", "user1"),
+		overflow.WithArg("maxEdition", 1),
+		overflow.WithArg("artist", "Neo"),
+		overflow.WithArg("nftName", "Motorcycle"),
+		overflow.WithArg("nftDescription", `Bringing the motorcycle world into the 21st century with cutting edge EV technology and advanced performance in a great classic British style, all here in the UK`),
+		overflow.WithArg("nftUrl", "https://neomotorcycles.co.uk/assets/img/neo_motorcycle_side.webp"),
+		overflow.WithArg("rarity", "rare"),
+		overflow.WithArg("rarityNum", 50.0),
+		overflow.WithArg("to", "user1"),
+	).
+		AssertSuccess(t)
 
-		otu.registerDandyInNFTRegistry()
+	otu.registerDandyInNFTRegistry()
 
+	t.Run("Should be able to get dandies by script", func(t *testing.T) {
 		result, err := otu.O.Script("getNFTCatalogItems",
 			overflow.WithArg("user", "user1"),
 			overflow.WithArg("collectionIDs", `{"A.f8d6e0586b0a20c7.Dandy.NFT" : [504,505,503,506,502,507]}`),
 		).GetWithPointer("/A.f8d6e0586b0a20c7.Dandy.NFT")
+
+		if err != nil {
+			panic(err)
+		}
+
+		autogold.Equal(t, result)
+
+	})
+
+	t.Run("Should be able to get soul bounded items by script", func(t *testing.T) {
+
+		otu.registerExampleNFTInNFTRegistry()
+
+		ids, err := otu.O.Script("getNFTCatalogIDs",
+			overflow.WithArg("user", otu.O.Address("account")),
+			overflow.WithArg("collections", `[]`),
+		).
+			GetWithPointer("/A.f8d6e0586b0a20c7.ExampleNFT.NFT/extraIDs")
+
+		assert.NoError(t, err)
+
+		typedIds, ok := ids.([]interface{})
+
+		if !ok {
+			panic(ids)
+		}
+
+		collectionIDs := fmt.Sprintf(`{"A.f8d6e0586b0a20c7.ExampleNFT.NFT" : [%s]}`, strings.Trim(strings.Replace(fmt.Sprint(typedIds), " ", ",", -1), "[]"))
+
+		result, err := otu.O.Script("getNFTCatalogItems",
+			overflow.WithArg("user", otu.O.Address("account")),
+			overflow.WithArg("collectionIDs", collectionIDs),
+		).
+			GetWithPointer("/A.f8d6e0586b0a20c7.ExampleNFT.NFT")
 
 		if err != nil {
 			panic(err)
