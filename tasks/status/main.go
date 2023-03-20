d8982d632f44fe810d6310f969d77cdc7c180da2
Bjarte Stien Karlsen <bjarte@bjartek.org>
2023-03-20 12:50:55 +0100

    updated status script

--- tasks/status/main.go
+++ tasks/status/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	. "github.com/bjartek/overflow"
+	"github.com/samber/lo"
 )
 
 func main() {
@@ -10,51 +14,47 @@ func main() {
 		WithNetwork(network),
 	)
 
-	o.Script(`
+	input := os.Args[1]
 
-import FindLeaseMarket from "../contracts/FindLeaseMarket.cdc"
-import FindMarket from "../contracts/FindMarket.cdc"
-pub fun main() : AnyStruct {
- let types =FindLeaseMarket.getSaleItemCollectionTypes()
+	user := o.Script("getStatus", WithArg("user", input))
 
- 
- let allTypes : [String]=[]
- for type in types {
- 	allTypes.append(FindMarket.getMarketOptionFromType(type))
- }
- return allTypes
-}
-`).Print()
+	var address string
+	err := user.MarshalPointerAs("/profile/address", &address)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	err = user.MarshalPointerAs("/paths", &paths)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("number of paths", len(paths))
+
+	pathChunks := lo.Chunk(paths, 30)
+
+	allReports := map[string][]uint64{}
+
+	for _, c := range pathChunks {
+		nfts := o.Script("getNFTIds", WithArg("address", address), WithArg("targetPaths", c))
+
+		var report map[string][]uint64
+		err := nfts.MarshalAs(&report)
+		if err != nil {
+			panic(err)
+		}
+
+		for col, ids := range report {
+			allReports[col] = ids
+		}
+
+	}
+
+	fmt.Println(len(allReports))
 	/*
-	   input := os.Args[1]
+		collection := "GaiaCollection001"
+		itemRes := o.Script("getNFTItems", WithArg("address", address), WithArg("collection", collection), WithArg("ids", allReports[collection]))
 
-	   	user := o.Script("getInfo", WithArg("user", input))
-
-	   	var address string
-	   	user.MarshalPointerAs("/profile/address", &address)
-
-	   	var paths []string
-	   	user.MarshalPointerAs("/paths", &paths)
-
-	   	pathChunks := lo.Chunk(paths, 25)
-
-	   	allReports := map[string][]uint64{}
-	   	for _, c := range pathChunks {
-	   		nfts := o.Script("getNFTIds", WithArg("address", address), WithArg("targetPaths", c))
-
-	   		var report map[string][]uint64
-	   		nfts.MarshalAs(&report)
-
-	   		for col, ids := range report {
-	   			allReports[col] = ids
-	   		}
-
-	   	}
-
-	   	litter.Dump(allReports)
-	   	collection := "GaiaCollection001"
-	   	itemRes := o.Script("getNFTItems", WithArg("address", address), WithArg("collection", collection), WithArg("ids", allReports[collection]))
-
-	   	itemRes.Print()
+		itemRes.Print()
 	*/
 }
