944fc3c4f1591c4693ba10454b3f8d33fd35fb37
Bjarte S. Karlsen <bjarte@find.xyz>
2023-04-01 10:46:02 +0200

    add lease for sale tx with new logic (#367)
    * add lease for sale tx with new logic
    
    * new version

---
+++ tasks/browse/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/bjartek/overflow"
+)
+
+func main() {
+
+	address := "0x8bf9ecc3a2b8d7af"
+	o := overflow.Overflow(overflow.WithNetwork("testnet"))
+
+	result := o.Script("getFindPaths", overflow.WithArg("user", address))
+
+	if result.Err != nil {
+		panic(result.Err)
+	}
+
+	result.Print()
+
+	var res Result
+	err := result.MarshalAs(&res)
+	if err != nil {
+		panic(err)
+	}
+
+	//pub fun main(address: Address, targetPaths: [String]): {String : Report}{
+	ids := o.Script("getNFTIDs", overflow.WithArg("address", address), overflow.WithArg("targetPaths", res.Paths))
+
+	ids.Print()
+
+}
+
+type Result struct {
+	Paths []string `json:"paths"`
+}
