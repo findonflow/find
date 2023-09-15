175dd943b554a75948296c3163d065ec297f39a4
Bjarte Stien Karlsen <bjarte@bjartek.org>
2023-09-15 12:46:35 +0200

    fix send vouchers

--- tasks/AirdropNameVoucher/main.go
+++ tasks/AirdropNameVoucher/main.go
@@ -9,9 +9,15 @@ func main() {
 	)
 
 	list := map[uint64][]string{
-		3: {"0x7f97054dc583b63c"},
-		4: {"0x7f97054dc583b63c"},
-		5: {"0x7f97054dc583b63c"},
+		3: {"0x380d59166ad2008d"},
+		5: {
+			"0x01e91f47de20ef05",
+			"0x357c4382737b46a1",
+			"0x76d988a29af9ea8d",
+			"0xf5abd0ab244b6a78",
+			"0x89a6febfce2215ae",
+			"0xe11f527c4c013938",
+		},
 	}
 
 	AirdropNameVoucher(list, o)
